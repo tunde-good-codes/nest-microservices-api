1cce4cce245f8af0d30f453f122b1efb4e2acaf2
tundenewcodes <fasasipelumi1652@gmail.com>
2026-02-18 23:59:24 +0100

    gateway setup new

--- apps/catalog/src/main.ts
+++ apps/catalog/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { CatalogModule } from './catalog.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CatalogModule);
-  await app.listen(process.env.port ?? 3000);
+  process.title = 'catalog';
+  const logger = new Logger('catalogbootstrap');
+  const port = Number(process.env.CATALOG_TCP_PORT ?? 4011);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    CatalogModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port,
+      },
+    },
+  );
+  await app.listen(port);
 }
 bootstrap();
